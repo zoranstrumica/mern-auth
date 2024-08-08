fdb8146212fefd7dc9e5b31884f334ce3c0841dd
Zoran Nikolov <zoranstrumica7@gmail.com>
2024-08-08 11:32:40 +0200

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,26 +4,34 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Conected to MongoDB");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("App listen on port 3000!");
+  console.log("Server listening on port 3000");
 });
 
 app.use("/api/user", userRoutes);
@@ -34,7 +42,7 @@ app.use((err, req, res, next) => {
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    error: message,
-    statusCode: statusCode,
+    message,
+    statusCode,
   });
 });
